08f5343fadec17b14c822d6a16d30dcea62c9cd2
vishalpande <vishalpande40@gmail.com>
2023-02-18 08:50:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+      
+         Developed by{" "}
+        <a target="_blank" href="">
+          Vishal Pande
+       
+        </a>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "cd98063da013df9caadaa3a2239a4c73",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
